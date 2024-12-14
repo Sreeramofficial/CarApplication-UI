cf69988e2d5e93ca54d57cba27513144b7dd24f5
Sreeramofficial <mssreerammanatt@gmail.com>
2024-12-14 18:04:57 +0530

    initialized all components

---
+++ car_application_ui/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/auth-components/login/login.component';
+import { SignupComponent } from './auth/auth-components/signup/signup.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: SignupComponent,
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./module/admin/admin.module').then((e) => e.AdminModule),
+  },
+  {
+    path: 'customer',
+    loadChildren: () =>
+      import('./module/customer/customer.module').then((e) => e.CustomerModule),
+  },
+];
